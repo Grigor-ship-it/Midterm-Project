93af7af63cf298b7c499f37a99b5338dd6b49c3d
Grigor Baltadjian <gb795@hotmail.com>
2021-05-02 17:37:51 +0000

    Menu on load feature is included

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 $( document ).ready(function() {
 
    $("#menuItemsButton").click(function(event) {
@@ -18,6 +12,35 @@ $( document ).ready(function() {
       }
     })
    })
+
+   $.ajax({
+    url: "/menu",
+    method: "GET",
+    success: (data) => {
+      let menuItems = data.menuItems
+      menuItems.forEach(item => {
+        $(".menu").append(`<div class=${item.id}> <img src=${item.display_image} style="width:100px;height:100px;">${item.name}</div>`)
+      })
+    }
+ })
+
+  /* $(`.${item.id}`).click(function(event) {
+    $.ajax({
+      url: "/menu",
+      method: "GET",
+      success: (data) => {
+        let menuItems = data.menuItems
+        menuItems.forEach(item => {
+          $(".menu").append(`<div>${item.name}</div>`)
+        })
+      }
+    })
+   })*/
+
+
+
+
+
 
 })
 
