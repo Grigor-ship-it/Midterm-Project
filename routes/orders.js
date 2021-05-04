4610c36cd7d1c7c11f2db135caebb3a389fcf36f
Mohamed Hassan <mhass405@gmail.com>
2021-05-04 09:26:07 +0000

    Add route for to query specific user_orders items

--- routes/orders.js
+++ routes/orders.js
@@ -32,5 +32,43 @@ module.exports = (db) => {
     });
   });
 
+  router.get("/cart", (req, res) => {
+    // getting specific user order (faking it for user 1)
+    db.query(`
+    SELECT * FROM orders
+    JOIN user_orders ON user_orders.id = user_orders_id
+    JOIN menu_items ON user_orders.item_id = menu_items.id
+    `)
+    // JOIN items ON menu_items.id = menu_item_id;
+    .then(data => {
+      const orders = data.rows;
+      res.json({ orders });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  });
+
+  router.get("/cart/:id", (req, res) => {
+    // getting specific user order (faking it for user 1)
+    db.query(`
+    SELECT * FROM user_orders
+    JOIN menu_items ON user_orders.item_id = menu_items.id
+    WHERE menu_items.id = $1
+    `, [req.params.id])
+    // JOIN items ON menu_items.id = menu_item_id;
+    .then(data => {
+      const orders = data.rows;
+      res.json({ orders });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  });
+
   return router;
 };
